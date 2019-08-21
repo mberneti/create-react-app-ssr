4fea9e964b07ffe4e7c6fd21a3bd5611777fe606
Mohammadreza Berneti <mberneti@live.com>
2019-08-21 15:53:52 +0430

    .

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/home';
 import Counter from './components/counter';
+import Menu from './components/menu';
 
 function AppRouter() {
   return (
     <Router>
+      <Menu />
       <Route path="/" exact component={Home} />
-      <Route path="/counter/" component={Counter} />
+      <Route path="/counter" component={Counter} />
     </Router>
   );
 }
